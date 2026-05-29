68c7589b8efa8c350d52a80e4cea7dac8ee55fc1
Luis Enrique <luisenriquesinisterraperez@gmail.com>
2026-05-29 15:15:18 -0500

    cambia puerto BD de 3306 a 3307

--- config/app.php
+++ config/app.php
@@ -292,7 +292,7 @@ return [
             'timezone' => 'UTC',
 
             'host' => env('DATABASE_HOST', 'localhost'),
-            'port' => env('DATABASE_PORT', '3306'),
+            'port' => env('DATABASE_PORT', '3307'),
             'username' => env('DATABASE_USER', 'root'),
             'password' => env('DATABASE_PASSWORD', ''),
             'database' => env('DATABASE_NAME', 'cake'),
